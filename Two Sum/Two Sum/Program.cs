ffb3504e9286ce77592ce81c830a0e044cce418a
Talal Khan <FA20-BCS-029@cuiatd.edu.pk>
2024-08-09 20:18:42 +0500

    Two Sum problem solved

---
+++ Two Sum/Two Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Two_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, };
+            int target = 4;
+            TwoSum(num, target);
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                for (int j = i+1; j<nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j]) == target)
+                    {
+                        int[] indices = { i,j};
+                        return indices;
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+}
