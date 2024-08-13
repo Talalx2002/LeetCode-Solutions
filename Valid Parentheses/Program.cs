58091b3a7262d79ea07771f2e2886bcf0a128432
Talal Khan <FA20-BCS-029@cuiatd.edu.pk>
2024-08-13 14:35:11 +0500

    Valid Parantheses

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Valid_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string s = "[";
+
+            Console.WriteLine(isValid(s));
+        }
+
+        public static bool isValid(string s)
+        {
+            Stack<char> myStack = new Stack<char>();
+            char Top;
+
+            for (int i = 0; i< s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                {
+                    myStack.Push(s[i]);
+                }
+                else if ((s[i] == ')' || s[i] == '}' || s[i] == ']'))
+                {
+                    if (myStack.Count == 0)
+                        return false;
+
+                    Top = myStack.Pop();
+
+                    if ((s[i] == ')' && Top != '(') || (s[i] == '}' && Top != '{') || (s[i] == ']' && Top != '['))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return myStack.Count == 0;
+        }
+    }
+}
