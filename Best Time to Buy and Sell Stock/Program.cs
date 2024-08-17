df0c793069438ff08f0f1237b9159114f36df96e
Talal Khan <FA20-BCS-029@cuiatd.edu.pk>
2024-08-17 20:43:57 +0500

    project setup done, working on logic

---
+++ Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Best_Time_to_Buy_and_Sell_Stock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] prices = { 1, 2, 4, 5, 6, 7, 8 };
+            int profit = 0;
+            profit = MaxProfit(prices);
+        }
+        public static int MaxProfit(int[] prices)
+        {
+            bool flag = false;
+
+            for(int i=0; i< prices.Length; i++)
+            {
+                if (flag)
+                {
+
+                }
+            }
+            return 0;
+        }
+    }
+}
+
